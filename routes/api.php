6ad1ed2b9c5ca05d623d9586c5926431f4047b18
66hakeem <66hakeem@gmail.com>
2022-12-14 14:36:12 +0000

    Loads data with vue options api

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('posts', [\App\Http\Controllers\Api\PostController::class, 'index']);
